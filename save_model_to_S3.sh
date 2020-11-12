f7c3ff9135f4713d548de763a114f202a7b4901f
Sascha <sascha.metzger@outlook.com>
2020-11-12 11:06:50 +0100

    initial commit

---
+++ save_model_to_S3.sh
@@ -0,0 +1,7 @@
+# chmod u+x save_model_to_S3.sh
+# s3cmd put File model/best.model.hdf5 s3://ml-scaleway-example/ --acl-public
+
+
+# s3cmd mb s3://ml-scaleway-example --acl-public
+
+# s3cmd put File data/Archive.zip s3://ml-scaleway-example/ --acl-public
