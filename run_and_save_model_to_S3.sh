855b88fcb5f22b8559c8044d0e2456e2a8d33a68
Sascha <sascha.metzger@outlook.com>
2020-11-13 11:16:29 +0100

    update sh scripts to use new S3 bucket

--- run_and_save_model_to_S3.sh
+++ run_and_save_model_to_S3.sh
@@ -1,6 +1,6 @@
 # chmod u+x run_and_save_model_to_S3.sh
 docker build -t mlscwexample .
 docker run --name mlscwexample --rm -v "$PWD":/app mlscwexample python -m model.model.py
-s3cmd put File model/best.model.hdf5 s3://ml-scaleway-example/ --acl-public
-s3cmd put File model/history.png s3://ml-scaleway-example/ --acl-public
-s3cmd put File model/my_model.json s3://ml-scaleway-example/ --acl-public
+s3cmd put File model/best.model.hdf5 s3://scw-ml-example --acl-public
+s3cmd put File model/history.png s3://scw-ml-example --acl-public
+s3cmd put File model/my_model.json s3://scw-ml-example --acl-public
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,5 @@
 # chmod u+x run_pipeline.sh
 docker build -t mlscwexample .
 docker run --name mlscwexample --rm -v "$PWD":/app mlscwexample python pipeline/run_pipeline.py
-s3cmd put File data/testing_set.csv s3://bernard-mobility-analyser --acl-public
-s3cmd put File data/training_set.csv s3://bernard-mobility-analyser --acl-public
+s3cmd put File data/testing_set.csv s3://scw-ml-example --acl-public
+s3cmd put File data/training_set.csv s3://scw-ml-example --acl-public
