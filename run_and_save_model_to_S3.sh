b36ee8676b08a1a81c8877c0f91dc51e7481e08b
Sascha <sascha.metzger@outlook.com>
2020-11-14 09:34:27 +0100

    update requirements + add git reset to .sh scripts

--- run_and_save_model_to_S3.sh
+++ run_and_save_model_to_S3.sh
@@ -1,5 +1,7 @@
 # chmod u+x run_and_save_model_to_S3.sh
-git fetch && git pull
+git fetch
+git reset --hard origin/main
+
 docker build -t mlscwexample .
 docker run --name mlscwexample --rm -v "$PWD":/app mlscwexample python -m model.model.py
 s3cmd put File model/best.model.hdf5 s3://scw-ml-example --acl-public
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,7 @@
 # chmod u+x run_pipeline.sh
-git fetch && git pull
+git fetch
+git reset --hard origin/main
+
 docker build -t mlscwexample .
 docker run --name mlscwexample --rm -v "$PWD":/app mlscwexample python pipeline/run_pipeline.py
 s3cmd put File data/testing_set.csv s3://scw-ml-example --acl-public
